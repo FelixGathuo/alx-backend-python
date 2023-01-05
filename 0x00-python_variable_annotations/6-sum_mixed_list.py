d246fd143a11303b9f35661247a990623c293351
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-05 15:05:25 +0300

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-""" Annotation for mixed list sum"""
+""" Complex types - mixed list """
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """Takes an input of combined list of integers
-    and floats and returns their sum as a float
-    """
-
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ type-annotated function sum_mixed_list which takes a list mxd_lst
+        of floats and integers and returns their sum as a float. """
     return sum(mxd_lst)
-
