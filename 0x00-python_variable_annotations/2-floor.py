f3d13e641b2a74064a389a83696fe13a6a02a42b
FelixGathuo <gathuofelix@gmail.com>
2023-01-05 11:52:09 +0000

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,9 +2,8 @@
 """Floor"""
 
 
-def floor(n: float) -> int
+def floor(n: float) -> int:
     """The function takes an input in form of float
-       and returns as int
-    """
+       and returns as int"""
     return int(n)
 
