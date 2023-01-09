6ffdc2cf6529d06d964c9e84a2bd37d3ae49037a
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-09 13:11:17 +0300

    concurrent coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+import asyncio
+'''an async routine called wait_n that imports the wait_random 
+coroutine from the previous Python file and takes in two integer
+arguments n and max_delay. It spawns the wait_random coroutine 
+n times with the specified max_delay, and returns the list of 
+all the delays in ascending order
+'''
+
+
+async def wait_n(n, max_delay):
+    delays = []
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+    return sorted(delays)
